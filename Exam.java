584f1887ef5b118bec3ad45402b7c90d41013fe3
woodcock-bailey <woodcock_bailey@students.columbusstate.edu>
2026-05-11 02:21:26 -0400

    Update Exam.java

--- Exam.java
+++ Exam.java
@@ -13,6 +13,17 @@ public class Exam extends Assessment {
     private int numMissed;
 
     public Exam(int questions, int missed) {
+         if (questions <= 0) {
+            throw new IllegalArgumentException("The number of questions must be greater than 0.");
+        }
+
+        if (missed < 0) {
+            throw new IllegalArgumentException("The number missed cannot be negative.");
+        }
+
+        if (missed > questions) {
+            throw new IllegalArgumentException("The number missed cannot be greater than the number of questions.");
+        }
         numQuestions = questions;
         numMissed = missed;
         pointsEach = 100.0 / questions;
